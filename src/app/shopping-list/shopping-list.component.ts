3383da60c07536824be931cd314959d0f4095820
helangor <henrikki_helander@hotmail.com>
2020-10-12 19:51:52 +0300

    Separated recipe and shopping list views

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
-import { Ingredient } from '../shopping-list.component.model';
-
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
